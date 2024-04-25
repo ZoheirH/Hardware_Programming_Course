3e693502136f704e3854633bf02b7ae0c6506ae0
ZoheirH <117117791+ZoheirH@users.noreply.github.com>
2024-04-25 16:09:21 +0330

    Add files via upload

---
+++ Arduino/info.txt/info.txt.ino
@@ -0,0 +1,12 @@
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
+}
